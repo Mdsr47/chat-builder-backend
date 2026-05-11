daff82c538628c80944e29cd93ec99a13feb0764
Mdsr47 <mdsr1850@gmail.com>
2026-05-11 12:45:35 +0500

    cors error

--- main.py
+++ main.py
@@ -40,6 +40,15 @@ from fastapi.staticfiles import StaticFiles
 
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 # app.mount("/static", StaticFiles(directory="static"), name="static")
 if os.path.exists("static"):
     app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -60,13 +69,13 @@ async def privacy_policy(request: Request):
     )
 
 # CORS
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000", "http://localhost:8000", "http://localhost:8080"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["http://localhost:5173", "http://localhost:3000", "http://localhost:8000", "http://localhost:8080"],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 load_index()
 
